26f0ac20b127c63ab3e460c1190a23d4b8d44323
aparnagupta <aparnagupta474@gmail.com>
2025-10-14 13:28:01 +0530

    added a new leetcode problem that is problem 35

---
+++ Arrays/Leetcode35.java
@@ -0,0 +1,19 @@
+public class Leetcode35 {
+
+    public static int findPos(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>=target){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 3, 5, 6 };
+        int target = 2;
+        int result = findPos(nums, target);
+        System.out.println(result);
+
+    }
+}
